06c277b35b3f114c2c7de109e66b03100d806a0c
SalaAtsonic <Salaretro@hotmail.com>
2024-11-19 13:55:53 +0100

    Added city input and database validation.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 // Recommended: All functions declared here
+function getCityName () {
+    return prompt("Enter the name of a city")
+}
+
+const cityName = getCityName ();
+document.querySelector("h2").textContent = cityName;
+
+function isCityInDatabase(cityName) {
+    for (let i = 0; i < cities.length; i++) {
+        if (cities[i].name.toLowerCase() === cityName.toLowerCase()) {
+            return cities[i];
+        }
+    }
+    document.querySelector("h2").textContent = cityName +  "finns inte i databasen.";
+}
+const cityExists = isCityInDatabase(cityName);
+
+document.querySelector("h2").textContent = `${cityExists.name} (${cityExists.country})`;
 
 // Recommended: constants with references to existing HTML-elements
 
 // Recommended: Ask for the city name and then the rest of the code
-
